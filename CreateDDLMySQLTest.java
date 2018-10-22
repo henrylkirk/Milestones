c3f6f6cd8ee03c3d0ded9a737d63c6725c3e1117
henrylkirk <henry@the-kirks.com>
2018-10-22 13:39:25 -0400

    Added skeleton of first test class

---
+++ CreateDDLMySQLTest.java
@@ -0,0 +1,7 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CreateDDLMySQLTest {
+    @Test
+    public void testConvertStrBooleanToInt() {}
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Milestones
+
+javac -cp .:junit-4.12.jar CreateDDLMySQLTest.java
+javac CreateDDLMySQLTest.java
