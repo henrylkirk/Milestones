dfd26f8032f3607f320a891ca89f4c1c0b6d10e9
rjs5176 <rjs5176@rit.edu>
2018-10-24 14:13:14 -0400

    Update EdgeConnectorTest.java
    All test methods created

--- EdgeConnectorTest.java
+++ EdgeConnectorTest.java
@@ -2,16 +2,43 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class EdgeConnectorTest {
-    int numConnector = 1
+    String numConnector = "1";
+    String endPoint1 = "2";
+    String endPoint2 = "3";
+    String endStyle1 = "Test1";
+    String endStyle2 = "Test2";
     
-    EdgeConnector ec = new EdgeConnector(numConnector + DELIM + endPoint1 + DELIM + endPoint2 + DELIM + endStyle1 + DELIM + endStyle2);
+    EdgeConnector ec = new EdgeConnector(numConnector + "|" + endPoint1 + "|"+ endPoint2 + "|" + endStyle1 + "|" + endStyle2);
     
-    //@Test
-    //public void testGetNumConnector()
-    //public void testGetEndPoint1
-    //public void testGetEndPoint2
-    //public void testGetEndStyle1
-    //public void testGetEndStyle2
+    @Test
+    public void testGetNumConnector(){
+      int numConnect = ec.getNumConnector();
+      assertEquals(1,numConnect);
+    }
+    
+    @Test
+    public void testGetEndPoint1(){
+      int point1 = ec.getEndPoint1();
+      assertEquals(2,point1);
+    }
+    
+    @Test
+    public void testGetEndPoint2(){
+      int point2 = ec.getEndPoint2();
+      assertEquals(3,point2);
+    }
+    
+    @Test
+    public void testGetEndStyle1(){
+      String style1 = ec.getEndStyle1();
+      assertEquals("Test1",style1);
+    }
+    
+    @Test
+    public void testGetEndStyle2(){
+      String style2 = ec.getEndStyle2();
+      assertEquals("Test2",style2);
+    }
    
     @Test
     public void testIsEP1Field(){
@@ -23,7 +50,7 @@ public class EdgeConnectorTest {
     @Test
     public void testIsEP2Field(){
       ec.setIsEP2Field(true);
-      boolean ep2Field - ec.getIsEP2Field();
+      boolean ep2Field = ec.getIsEP2Field();
       assertEquals(true,ep2Field);
     }
     
