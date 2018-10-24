c1c18d82130eec0baf75cb0b8ca39b491676c4b0
henrylkirk <henry@the-kirks.com>
2018-10-24 15:04:49 -0400

    Added setUp and two tests for EdgeTable

--- EdgeTableTest.java
+++ EdgeTableTest.java
@@ -1,7 +1,33 @@
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import org.junit.Before;
+import java.io.*;
 
 public class EdgeTableTest {
-    // @Test
-    // public void TODO() {}
+
+    private EdgeConvertFileParser ecfp;
+    private File parseFile;
+    private EdgeTable[] tables;
+    private EdgeTable testTable;
+
+    @Before
+    public void setUp() {
+        parseFile = new File("Courses.edg");
+        ecfp = new EdgeConvertFileParser(parseFile);
+        tables = ecfp.getEdgeTables();
+        testTable = tables[0];
+    }
+
+    @Test
+    public void testGetNumFigure() {
+        int num = testTable.getNumFigure();
+        assertEquals(num,1);
+    }
+
+    @Test
+    public void testGetName() {
+        String name = testTable.getName();
+        assertEquals(name,"STUDENT");
+    }
+
 }
