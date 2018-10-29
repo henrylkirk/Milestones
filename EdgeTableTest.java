d4d7a6007dc0bcab256e43ce8b18d548c31bb7c0
henrylkirk <henry@the-kirks.com>
2018-10-29 13:26:39 -0400

    Added tests

--- EdgeTableTest.java
+++ EdgeTableTest.java
@@ -16,6 +16,7 @@ public class EdgeTableTest {
         ecfp = new EdgeConvertFileParser(parseFile);
         tables = ecfp.getEdgeTables();
         testTable = tables[0];
+        testTable.makeArrays();
     }
 
     @Test
@@ -28,6 +29,34 @@ public class EdgeTableTest {
     public void testGetName() {
         String name = testTable.getName();
         assertEquals(name,"STUDENT");
+    }
+
+    @Test
+    public void testGetRelatedTablesArray() {
+        testTable.addRelatedTable(99);
+        testTable.makeArrays();
+        int[] relatedTables = testTable.getRelatedTablesArray();
+        int relatedTable = relatedTables[0];
+        assertEquals(relatedTable,99);
+    }
+
+    @Test
+    public void testRelatedFields() {
+        testTable.setRelatedField(0,99);
+        int[] relatedFields = testTable.getRelatedFieldsArray();
+        int fieldNum = relatedFields[0];
+        assertEquals(fieldNum,99);
+    }
+
+    @Test
+    public void testNativeFields() {
+        int[] nativeFields = testTable.getNativeFieldsArray();
+        int initLength = nativeFields.length;
+        testTable.addNativeField(99);
+        testTable.makeArrays();
+        nativeFields = testTable.getNativeFieldsArray();
+        int finalLength = nativeFields.length;
+        assertEquals(initLength+1,finalLength);
     }
 
 }
