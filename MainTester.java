def8307877aa2d7197f3ea5ab6a4d77fe4606f38
henrylkirk <henry@the-kirks.com>
2018-11-19 11:37:23 -0500

    Added delim to MainTester to reduce coupling

--- MainTester.java
+++ MainTester.java
@@ -7,6 +7,7 @@ import java.util.*;
 public class MainTester {
 
     private String inputString;
+    private static final String DELIM = ",";
 
     MainTester(String inputString) {
         this.inputString = inputString;
@@ -22,7 +23,7 @@ public class MainTester {
     private void handleTestObject() {
 
         // Break up inputString
-        StringTokenizer st = new StringTokenizer(inputString, EdgeConnector.DELIM);
+        StringTokenizer st = new StringTokenizer(inputString, DELIM);
         int testNum = Integer.parseInt(st.nextToken());
         int testPoint1 = Integer.parseInt(st.nextToken());
         int testPoint2 = Integer.parseInt(st.nextToken());
